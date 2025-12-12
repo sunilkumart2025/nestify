fe369162c745a62a4b193c970f12eebccfadc7cc
sunilkumart2025 <suniltamil2025@gmail.com>
2025-12-12 19:17:11 +0530

    Updated UI files and payment logic

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -1,2 +1,33 @@
-Need to install the following packages:
-supabase@2.65.8
+export type Json =
+    | string
+    | number
+    | boolean
+    | null
+    | { [key: string]: Json | undefined }
+    | Json[]
+
+export interface Database {
+    public: {
+        Tables: {
+            [key: string]: {
+                Row: any
+                Insert: any
+                Update: any
+            }
+        }
+        Views: {
+            [key: string]: {
+                Row: any
+            }
+        }
+        Functions: {
+            [key: string]: {
+                Args: any
+                Returns: any
+            }
+        }
+        Enums: {
+            [key: string]: any
+        }
+    }
+}
